b120ad7f24990fa07b99f354c125fb583150729d
jasheshK <jashesh1803@gmail.com>
2024-09-01 12:27:25 +0530

    firstcommit

---
+++ backend.py
@@ -0,0 +1,18 @@
+import requests 
+
+API_KEY = "894f6de95631f5989b800f071c6e2fc5"
+
+
+def get_data(place, forecast_days):
+    url = f'https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place = "Tokyo", forecast_days=3))
